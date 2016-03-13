0a5191fd37b3566ba346dad5d32d688f77ea6e5d
suzimaru <suzannesorli@gmail.com>
2016-03-13 16:01:59 +0100

    Creation de tous les menus avec les fonctionnalités basiques (du genre retour quitter etc)

--- interface_graphique.c
+++ interface_graphique.c
@@ -65,14 +65,14 @@ void affichage_menu (int menu)
 		}
 		case (2):
 		{	
-			posQuitter.x=340;
+			posQuitter.x=335;
 			posQuitter.y=435;
 	
 			position_fond.x=0;
 			position_fond.y=0;
 	
-			posTexte_menu.x=350;		
-			posTexte_menu.y=75;
+			posTexte_menu.x=345;		
+			posTexte_menu.y=15;
 		
 			posTexte_auteur.x=255;
 			posTexte_auteur.y=540;
@@ -86,6 +86,9 @@ void affichage_menu (int menu)
 			posHvsIA2.x=285;
 			posHvsIA2.y=295;
 			
+			posIAvsIA.x=335;
+			posIAvsIA.y=85;
+			
 			posretour.x=340;
 			posretour.y=365;
 			
@@ -95,8 +98,9 @@ void affichage_menu (int menu)
 		  SDL_Color fontPurple = {0,51,102};// couleur du menu
 		  texte1 = TTF_RenderText_Blended(fontMenu,"Jeu HEX",fontPurple);// texte du petit rectangle
 			HvsH = TTF_RenderText_Blended(fontMenu,"Humain VS Humain",fontPurple);
-			HvsIA1 = TTF_RenderText_Blended(fontMenu,"Humain VS AI1",fontPurple);
-			HvsIA2 = TTF_RenderText_Blended(fontMenu,"Humain VS AI2",fontPurple);
+			HvsIA1 = TTF_RenderText_Blended(fontMenu,"Humain VS IA1",fontPurple);
+			HvsIA2 = TTF_RenderText_Blended(fontMenu,"Humain VS IA2",fontPurple);
+			IAvsIA = TTF_RenderText_Blended(fontMenu,"IA VS IA",fontPurple);
 			retour = TTF_RenderText_Blended(fontMenu,"Retour",fontPurple);
 			
 			texte_auteur = TTF_RenderText_Blended(fontAuteur,"Garrigues, Perruchet, Sorli 2016",fontPurple);
@@ -113,6 +117,7 @@ void affichage_menu (int menu)
 	
 			SDL_BlitSurface(image_fond,NULL,ecran,&position_fond); // coller l'image de fond
 			SDL_BlitSurface(HvsH,NULL,ecran,&posHvsH);
+			SDL_BlitSurface(IAvsIA,NULL,ecran,&posIAvsIA);
 			SDL_BlitSurface(HvsIA1,NULL,ecran,&posHvsIA1);
 			SDL_BlitSurface(quitter,NULL,ecran,&posQuitter);
 			SDL_BlitSurface(HvsIA2,NULL,ecran,&posHvsIA2); // celle la après pour qu'elle ne soit pas écrasée
@@ -121,6 +126,40 @@ void affichage_menu (int menu)
 			SDL_BlitSurface(retour,NULL,ecran,&posretour);
 		  SDL_Flip(ecran);
 		  break;
+		}
+		case (3):
+		{
+			
+			posNiveau.x=140;
+			posNiveau.y=65;
+			
+			pos1IA.x=165;
+			pos1IA.y=220;
+			
+			pos2IA.x=165;
+			pos2IA.y=350;
+			
+			posretour.x=165;
+			posretour.y=475;
+			
+			TTF_Font *fontAuteur = TTF_OpenFont("hacked/hacked.ttf",22);// police du menu
+			TTF_Font *fontMenu = TTF_OpenFont("hacked/hacked.ttf",40);// police du menu
+			SDL_Color fontPurple = {0,51,102};// couleur du menu
+			
+			niveau = TTF_RenderText_Blended(fontMenu,"Quel est le niveau des IA ?",fontPurple);
+			niveau_premier_IA = TTF_RenderText_Blended(fontMenu,"Premiere IA :          1          2",fontPurple);
+			niveau_deuxieme_IA = TTF_RenderText_Blended(fontMenu,"Deuxieme IA :          1          2",fontPurple);
+			retour = TTF_RenderText_Blended(fontMenu,"Retour",fontPurple);
+			
+			
+			SDL_BlitSurface(image_fond,NULL,ecran,&position_fond);
+			SDL_BlitSurface(niveau,NULL,ecran,&posNiveau);
+			SDL_BlitSurface(niveau_premier_IA,NULL,ecran,&pos1IA);
+			SDL_BlitSurface(niveau_deuxieme_IA,NULL,ecran,&pos2IA);
+			SDL_BlitSurface(retour,NULL,ecran,&posretour);
+			SDL_Flip(ecran);
+			
+			break;
 		}
 	}
 }
@@ -141,6 +180,7 @@ void clean (int menu)
   		SDL_FreeSurface(jouer);
   		SDL_FreeSurface(charger);
   		SDL_FreeSurface(quitter);
+
 			break;
 		}
 		case (2):
@@ -149,14 +189,26 @@ void clean (int menu)
   		SDL_FreeSurface(HvsH);
   		SDL_FreeSurface(texte_menu);
   		SDL_FreeSurface(texte_auteur);
+  		SDL_FreeSurface(IAvsIA);
   		SDL_FreeSurface(HvsIA1);
   		SDL_FreeSurface(HvsIA2);
   		SDL_FreeSurface(quitter);		
   		SDL_FreeSurface(retour);
-SDL_FreeSurface(ecran);	
+			SDL_FreeSurface(ecran);	
+
   		break;
 		}
-}
+		case (3):
+		{
+			SDL_FreeSurface(niveau);
+			SDL_FreeSurface(niveau_deuxieme_IA);
+			SDL_FreeSurface(niveau_premier_IA);
+			SDL_FreeSurface(retour);
+			SDL_FreeSurface(ecran);
+
+			break;
+		}
+	}
   TTF_CloseFont(fontMenu);
   TTF_CloseFont(fontAuteur);
 }
@@ -216,69 +268,152 @@ void affichage ()
 /////////////////////////////////////// BOUCLE PRINCIPALE EVENT ////////////////////////////////////////:
    
    
-    continuer = true;
-    SDLKey key_pressed;
-   	affichage_menu(1);
-   	
-    while (continuer)
-  	{
-        while ( SDL_PollEvent(&event))
+  continuer = true;
+  SDLKey key_pressed;
+  affichage_menu(1);
+  int menu=1;
+  while (continuer)
+  {
+  	while ( SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_MOUSEMOTION:
+          printf("Ça bouge\n");
+          break;
+        case SDL_MOUSEBUTTONDOWN:
         {
-            switch (event.type)
-            {
-                case SDL_MOUSEMOTION:
-                    printf("Ça bouge\n");
-                    break;
-                case SDL_MOUSEBUTTONDOWN:
-                    if (event.button.button == SDL_BUTTON_LEFT)
-                    {  
-                    	Clic c; 
-                        c.x = event.motion.x;
-                        c.y = event.motion.y;
-                        printf("X=%d Y=%d\n",c.x,c.y); // on récupère les coordonnées du clic
+          if (event.button.button == SDL_BUTTON_LEFT)
+          {  
+           	Clic c; 
+            c.x = event.motion.x;
+            c.y = event.motion.y;
+            printf("X=%d Y=%d\n",c.x,c.y); // on récupère les coordonnées du clic
                      
-                       //if ((clicX>=400  && clicX<=600) && (clicY>=180 && clicY<=250))
-                       int jouer;
-                        if (jouer=clic_Valide(c,330,240,450,295))
-                        {
-                        	clean(1);
-                        	affichage_menu(2);
-                        }
-                    	}
-                    break;
-                case SDL_KEYDOWN:
-                    key_pressed = event.key.keysym.sym; // on récupère la touche
-                    switch (key_pressed)
-                    {
-                    case SDLK_ESCAPE: /* Esc keypress quits the app... */
-                        continuer = false;
-                        break;
-                    case SDLK_LEFT:
-                        printf("left +1\n");
-                        break;
-                    case SDLK_RIGHT:
-                        printf("right +1\n"); 
-                        break;
-                    case SDLK_UP:
-                        printf("up +1\n");
-                        break;
-                    case SDLK_DOWN:
-                        printf("down +1\n");
-                        break;
-                    }
-                    break;
-                case SDL_QUIT:
-                    continuer = false;
-                    break;
-                default:
-                    break;
-            }
-        	}
-        }
+            //if ((clicX>=400  && clicX<=600) && (clicY>=180 && clicY<=250))
+            switch (menu)
+            {
+////////////////////////////////////// MENU PRINCIPAL /////////////////////////////////////////////
+            	case (1):
+	            {
+	            	if (clic_Valide(c,330,240,450,295))
+              	{
+              		// Jouer
+              		clean(menu);
+              		menu++;
+               		affichage_menu(menu);
+               		break;
+              	}
+              	else if (clic_Valide(c,330,450,460,485))
+              	{
+              		// Quitter
+              		continuer=false;
+               		clean(menu);
+               		break;
+              	}
+              	else if (clic_Valide(c,315,340,470,385))
+              	{
+              		// Charger
+              		printf("charger\n");
+              		break;
+              	}
+              	break;
+              }
+/////////////////////////////////// DEUXIEME MENU ///////////////////////////////////////
+            	case (2):
+            	{
+            		
+								if (clic_Valide(c,330,450,460,485))
+              	{
+              		// QUITTER
+              		continuer=false;
+               		clean(menu);
+               		break;
+              	}
+            		else if (clic_Valide(c,330,360,455,400))
+            		{
+            			// RETOUR
+            			clean(menu);
+            			menu--;
+            			affichage_menu(menu);
+            			break;
+            		}
+            		else if (clic_Valide(c,325,80,450,120))
+            		{
+            			//IA VS IA 
+            			printf("menu =%d\n",menu);
+            			clean(menu);
+            			menu++;
+            			affichage_menu(menu);
+            			break;
+            		}
+            		else if (clic_Valide(c,250,150,550,190))
+            		{
+            		// H VS H
+            			printf("Humain vs Humain\n");
+            			break;
+            		}
+            		else if (clic_Valide(c,275,225,500,265))
+            		{
+            			// H VS IA1
+            			printf("Humain vs IA1\n");
+            			break;
+            		}
+            		else if (clic_Valide(c,280,300,500,330))
+            		{
+            			// H VS IA2
+            			printf("Humain vs IA2\n");
+            			break;
+            		}
+            	}
+/////////////////////////////////// TROISIEME MENU ///////////////////////////////////////
+            	case (3):
+            	{
+            		if (clic_Valide(c,160,470,285,515))
+            		{
+            			// RETOUR
+            			clean(menu);
+            			menu--;
+            			affichage_menu(menu);
+            		}
+            		break;
+            	}
+          	}
+          }
+        }  
+        case SDL_KEYDOWN:
+        key_pressed = event.key.keysym.sym; // on récupère la touche
+        switch (key_pressed)
+          {
+            case SDLK_ESCAPE: /* Esc keypress quits the app... */
+            	continuer = false;
+            	break;
+            case SDLK_LEFT:
+              printf("left +1\n");
+              break;
+            case SDLK_RIGHT:
+              printf("right +1\n"); 
+              break;
+            case SDLK_UP:
+              printf("up +1\n");
+              break;
+            case SDLK_DOWN:
+              printf("down +1\n");
+              break;
+          }
+          break;
+        case SDL_QUIT:
+        	continuer = false;
+          break;
+        default:
+          break;
+      }
+		}
+	}
+	
 
-/////////////////////////////////////////// CLEAN //////////////////////////////////////////
+/////////////////////////////////////////// QUIT //////////////////////////////////////////
    
-		clean(2);
 
     TTF_Quit();
     SDL_Quit();
--- interface_graphique.h
+++ interface_graphique.h
@@ -15,7 +15,7 @@
 #define WINHI 600 // window height
 #define WINWI 800 // window width
  
-SDL_Rect position_fond,posTexte1,posTexte_menu,posTexte_auteur,posJouer, posCharger, posQuitter, posHvsH, posHvsIA1, posHvsIA2, posretour;
+
 
 typedef struct
 {
@@ -26,12 +26,17 @@ typedef struct
 {
 	int x,y;
 }	Clic;
- SDL_Surface *ecran, *texte1, *texte_menu, *texte_auteur, *jouer, *charger, *quitter, *image_fond, *HvsH, *HvsIA1, *HvsIA2, *retour; // quelques surfaces
-  Dimensions fenetre; // main window
+
+
+SDL_Surface *ecran, *texte1, *texte_menu, *texte_auteur, *jouer, *charger, *quitter, *image_fond, *HvsH, *HvsIA1, *HvsIA2, *IAvsIA ,*retour,  *niveau_premier_IA, *niveau_deuxieme_IA, *niveau; 
+
+SDL_Rect position_fond,posTexte1,posTexte_menu,posTexte_auteur,posJouer, posCharger, posQuitter, posHvsH, posHvsIA1, posHvsIA2, posIAvsIA ,posretour, posNiveau, pos1IA, pos2IA ;
+
+Dimensions fenetre; // main window
 
 
 int clic_Valide (Clic c, int borneInfX, int borneInfY, int borneSupX, int borneSupY);
 void affichage (); 
 void clean (int menu);
-void affichage_menu_principal(int menu);
+void affichage_menu(int menu);
 #endif
